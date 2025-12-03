993c81b37c5e00a37e8f5b81ea9b370193a7883c
a.23308051280237 <a.23308051280237@cbtis128.edu.mx>
2025-12-03 22:49:34 +0000

    mecanico

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
--- devserver.sh
+++ devserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source .venv/bin/activate
-python mysite/manage.py runserver $PORT
+python backend/manage.py runserver $PORT
